218693301b64bdb0bd9c3fd0280a2f2aace89b72
BryanReese <bryan_reese209@yahoo.com>
2017-08-22 19:40:28 -0700

    tic tac toe

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-// wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+
+let c=0;
+
+$('.box').on('click', function() { 
+	c++;
+
+if (c%2===0 && $(this).text()!=="O"){
+$(this).append('<h1 class="xsos">X</h1>');
+	$('.disp').text("O's turn");
+
+} 
+else if(c%2!==0 && $(this).text()!=="X"){
+$(this).append('<h1 class="xsos">O</h1>');
+	$('.disp').text("X's turn");
+
+} else{
+	alert('This is already taken');
+	c=c-1;
+
+}
+
+});
+
+$('#res').on('click', function(event) {
+window.location = window.location;
+});
 
 });
